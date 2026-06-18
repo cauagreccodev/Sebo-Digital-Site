0d64704e7f3ed115d5cf06adfa5e6caf2bdbb46f
cauagreccodev <cauagrecco11@gmail.com>
2026-06-17 22:30:06 -0300

    fix: optimize catalog database loading

--- backend/sebodigital-api/src/main/java/br/com/sebodigital/api/repository/LivroRepository.java
+++ backend/sebodigital-api/src/main/java/br/com/sebodigital/api/repository/LivroRepository.java
@@ -1,16 +1,23 @@
 package br.com.sebodigital.api.repository;
 
 import br.com.sebodigital.api.model.entity.Livro;
+import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.domain.Specification;
 
 public interface LivroRepository extends JpaRepository<Livro, Long>, JpaSpecificationExecutor<Livro> {
 
     boolean existsByIsbnIgnoreCase(String isbn);
 
     Optional<Livro> findByIsbnIgnoreCase(String isbn);
+
+    @Override
+    @EntityGraph(attributePaths = {"editora", "vendedora", "copias", "copias.vendedor"})
+    List<Livro> findAll(Specification<Livro> spec, Sort sort);
 
     @Override
     @EntityGraph(attributePaths = {"editora", "vendedora", "copias", "copias.vendedor"})
